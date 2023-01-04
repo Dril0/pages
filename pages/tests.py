2bf9ba1209d24059a405d69fb7bb3c892d2e556d
Jeremias <jeremias.acurso@hotmail.com>
2023-01-04 13:40:08 -0300

    Creacion de test para la respuesta http exitosa a un http request

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import (
+    SimpleTestCase,
+)  # simple porque no vamos a usar por ahora una base de datos.
 
 # Create your tests here.
+
+"""Test para que las url retornen un http status code 200."""
+
+
+class HomepageTests(SimpleTestCase):
+    def test_url_exist_at_corrent_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+
+class AboutpageTests(SimpleTestCase):
+    def test_url_exist_at_corrent_location(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
